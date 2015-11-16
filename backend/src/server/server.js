c3d31165b2077b2379dc3aaf53889e7a1be0684f
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2015-11-16 11:58:10 +0000

    Using really-need library
    We override require to bust the npm cache so we get a new server each time, but only for our tests. Read http://glebbahmutov.com/blog/how-to-correctly-unit-test-express-server/

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -11,3 +11,5 @@ var server = app.listen(3000, function () {
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+module.exports = server;
--- backend/test/api/api.spec.js
+++ backend/test/api/api.spec.js
@@ -1,12 +1,21 @@
 'use strict';
 
 var request = require('supertest');
+require = require('really-need');
 
-request = request('http://localhost:3000');
+var server;
+
+beforeEach(function () {
+  server = require('../../src/server/server', { bustCache: true });
+});
+
+afterEach(function (done) {
+  server.close(done);
+});
 
 describe('API', function () {
   it('should return HTTP status code 200 for GET requests to root path', function (done) {
-    request
+    request(server)
       .get('/')
       .expect(200, done);
   });
