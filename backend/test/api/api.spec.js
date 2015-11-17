641d9c8002495f2d0f19e43c5e2bfd6bd3aed227
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2015-11-17 12:29:11 +0000

    test(api): adding api test

--- backend/test/api/api.spec.js
+++ backend/test/api/api.spec.js
@@ -14,9 +14,17 @@ afterEach(function (done) {
 });
 
 describe('API', function () {
+
   it('should return HTTP status code 200 for GET requests to root path', function (done) {
     request(server)
       .get('/')
       .expect(200, done);
   });
+
+  it('should return HTTP status code 404 for any request that is not a GET to the root path', function (done) {
+    request(server)
+      .get('/a')
+      .expect(404, done);
+  });
+
 });
